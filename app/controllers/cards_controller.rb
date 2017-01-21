47e828ee94691841c42f376d79a110a8596ac314
Jonah Price <email": null>
2017-01-20 16:26:45 -0800

    Add controller skelletons.

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,30 @@
+get '/cards' do
+
+end
+
+get '/cards/new' do
+
+end
+
+post '/cards' do
+
+end
+
+
+get '/cards/:id' do
+
+end
+
+
+get '/cards/:id/edit' do
+
+end
+
+put '/cards/:id' do
+
+end
+
+
+delete '/cards/:id' do
+
+end
---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,30 @@
+get '/groups' do
+  @groups
+end
+
+get '/groups/new' do
+
+end
+
+post '/groups' do
+
+end
+
+
+get '/groups/:id' do
+
+end
+
+
+get '/groups/:id/edit' do
+
+end
+
+put '/groups/:id' do
+
+end
+
+
+delete '/groups/:id' do
+
+end
